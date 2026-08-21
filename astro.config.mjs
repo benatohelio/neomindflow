8f14fb70039a6e4fb5dbf551bb3dec9f6a930945
benatohelio <benato.helio@gmail.com>
2026-08-20 22:15:34 -0300

    feat: BookCard with Open Library covers + ItemList schema (MDX)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://blog.neomindflow.cloud',
   base: '/',
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   trailingSlash: 'ignore',
 });
